bb4a7b31bb91524230a13cc52db96c695e0ceb67
kiraaziz <kiraaziz2020@gmail.com>
2023-12-26 15:08:27 +0100

    analytic

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,10 +1,12 @@
 import MainLayout from "@/components/MainLyout"
 import "@/styles/globals.css"
+import { Analytics } from '@vercel/analytics/react'
 
 const Layout = ({ children }) => {
 
     return (
         <MainLayout>
+            <Analytics />
             {children}
         </MainLayout>
     )
