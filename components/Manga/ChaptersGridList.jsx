60b82efe3ef8a49f02761b847062ed603ebb9f79
kiraaziz <83905214+kiraaziz@users.noreply.github.com>
2023-12-26 16:33:27 +0100

    Update ChaptersGridList.jsx

--- components/Manga/ChaptersGridList.jsx
+++ components/Manga/ChaptersGridList.jsx
@@ -9,6 +9,7 @@ const ChapterGridList = ({ chapterList, id, ch }) => {
         <div className=" grid grid-cols-1 lg:grid-cols-3 gap-2 py-3 lg:px-3 overflow-auto lg:h-[80svh]">
             {chapterList.map((val) => (
                 <Link
+                    rel="noopener noreferrer" target="_blank"
                     href={`/manga/${id}/${val.id}`}
                     className={` text-white rounded hover:bg-red-400 ease-in-out duration-500 p-3 ${ch === val.id ? "bg-red-400" : "bg-base-300"}`}>
                     {val.name}
