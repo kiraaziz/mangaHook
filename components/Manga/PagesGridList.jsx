81cb0b049127bca151e291e07c72381578d0e5ae
kiraaziz <83905214+kiraaziz@users.noreply.github.com>
2023-12-26 16:39:46 +0100

    Update PagesGridList.jsx

--- components/Manga/PagesGridList.jsx
+++ components/Manga/PagesGridList.jsx
@@ -8,13 +8,14 @@ const PagesGridList = ({ chapterList, id, ch }) => {
             <div className=" z-10 top-0 right-0  w-full flex gap-1 flex-row h-max overflow-x-scroll">
                 {chapterList.chapterListIds.reverse().map((val) => (
                     <Link
+                        rel="noopener noreferrer" target="_blank"
                         href={`/manga/${id}/${val.id}`}
                         className={` text-white rounded hover:bg-red-400 ease-in-out duration-500 p-3 ${ch === val.id ? "bg-red-400" : "bg-base-300"} whitespace-nowrap w-max`}>
                         {val.name}
                     </Link>
                 ))}
             </div>
-            <div className="h-max lg:h-[65svh] w-full overflow-y-scroll">
+            <div className="h-max w-full overflow-y-scroll">
                 <ZoomImages images={chapterList.images} />
             </div>
         </div>
